5440e07d34ab16db06f0dd215c7578c13086a098
Goodale2 <goodale.tanner@gmail.com>
2025-02-01 12:23:19 -0600

    refac: basename was wrong...

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router basename="/fordland-fitness-static">
+    <Router basename="/">
       <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
